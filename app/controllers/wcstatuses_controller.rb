5cd7d0c57462260ef4619708c1988f2bea50badb
Adam Skołuda <vexhelel@gmail.com>
2017-10-14 20:58:45 +0200

    Add Slack bot

--- app/controllers/wcstatuses_controller.rb
+++ app/controllers/wcstatuses_controller.rb
@@ -5,7 +5,20 @@ class WcstatusesController < ApplicationController
   end
 
   def create
+    client = Slack::Web::Client.new
     puts params.inspect
+
+    all_members = []
+
+    client.users_list(presence: true, limit: 10) do |response|
+      all_members.concat(response.members)
+    end
+
+    puts all_members
+
+    client = Slack::Web::Client.new
+
+    client.chat_postMessage(channel: '#devcamp-hackathon', text: 'Ktos zajął toaletę!', as_user: true)
     wcstatus = Wcstatus.create(params[:wctatus])
     puts wcstatus.is_busy
     render json: wcstatus
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module ToiletTrackerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    Slack.configure do |config|
+      config.token = 'xoxb-256417289810-JRSu3pU3B6fgTjKjQrAjutnw'
+    end
   end
 end
